d8a9a22b06f1efae37547d5714ec74811e3009f3
Alexander <degtyaralexander@gmail.com>
2024-06-17 23:51:33 +0300

    addded basic entities of feedmicroservice

---
+++ src/main/java/com/feedmicroservice/entity/CommentEntity.java
@@ -0,0 +1,64 @@
+package com.feedmicroservice.entity;
+
+import java.util.UUID;
+
+import com.usermicroservice.entity.UserEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class CommentEntity {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  UUID id;
+
+  @ManyToOne
+  SocialMediaEntity socialMediaEntity;
+
+  @ManyToOne
+  UserEntity publisher;
+
+  String content;
+
+  public CommentEntity(String content) {
+    this.content = content;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public SocialMediaEntity getSocialMediaEntity() {
+    return socialMediaEntity;
+  }
+
+  public void setSocialMediaEntity(SocialMediaEntity socialMediaEntity) {
+    this.socialMediaEntity = socialMediaEntity;
+  }
+
+  public UserEntity getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(UserEntity publisher) {
+    this.publisher = publisher;
+  }
+}
