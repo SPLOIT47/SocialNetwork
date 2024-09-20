9875c21a7f84c2007875f39c5905ac0763ce564c
sploit47 <degtyaralexander@gmail.com>
2024-09-20 14:41:09 +0300

    addded messenger/src && pre-commit codestyle check test

---
+++ social-network-frontend/src/components/header-nav/HeaderNav.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './../styles/HeaderNav.css'
+import './../styles/variables.css';
+import HeaderNavLogo from "./header-nav-components/HeaderNavLogo";
+import HeaderNavSearch from "./header-nav-components/HeaderNavSearch";
+import HeaderNavNotification from "./header-nav-components/HeaderNavNotification";
+import HeaderNavProfile from "./header-nav-components/HeaderNavProfile";
+
+const HeaderNav = () => {
+    const [activeMenu, setActiveMenu] = React.useState(null);
+    const toggleMenu = (menuName) => {
+        setActiveMenu(activeMenu === menuName? null : menuName);
+    }
+
+    return (
+        <div className='page-header-container'>
+            <div className='page-header-wrapper'>
+                <header className='page-header'>
+                    <ul className="header-nav">
+                        <HeaderNavLogo/>
+                        <HeaderNavSearch/>
+                        <HeaderNavNotification activeMenu={activeMenu} toggleMenu={toggleMenu} />
+                        <HeaderNavProfile activeMenu={activeMenu} toggleMenu={toggleMenu} />
+                    </ul>
+                </header>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default HeaderNav;
--- social-network-frontend/src/components/header-nav/header-nav-components/HeaderNavNotification.jsx
+++ social-network-frontend/src/components/header-nav/header-nav-components/HeaderNavNotification.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const HeaderNavNotification = () => {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    }
+const HeaderNavNotification = ({activeMenu, toggleMenu}) => {
+    const menuId = 'notification-menu';
+   const isMenuOpen = activeMenu === menuId;
 
     return (
         <li className='hv-notification-container'>
-            <button className='hv-notification-button' onClick={toggleMenu}>
+            <button className='hv-notification-button' onClick={() => toggleMenu(menuId)}>
                 <i className='fas fa-solid fa-bell hv-notification-icon'></i>
             </button>
             {isMenuOpen && (
